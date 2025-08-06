25fb9e739457cc0c2a9c31e3886d50742239f29a
ameliachapman11 <ameliachapman06@gmail.com>
2025-08-06 09:00:58 -0400

    Added background & lines

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # tic-tac-toe
 
-A simple tic tac toe game to learn the basics of initializing a screen and running a game using Pygame
+Picnic Tac Toe: A picnic themed twist on Tic Tac Toe using Pygame
--- game.py
+++ game.py
@@ -1,11 +1,32 @@
-import pygame
+import pygame, sys
 pygame.init
-screen = pygame.display.set_mode((800, 800))
-pygame.display.set_caption("Tic Tac Toe")
+
+#Initializing screen & images
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption("Picnic Tac Toe")
+background = pygame.image.load('picnicblanket.jpg')
+
+#Board lines
+LINE_COLOR = (91, 58, 28)
+LINE_WIDTH = 18
+def draw_lines():
+    #Horizontal lines
+    pygame.draw.line(screen, LINE_COLOR, (0, 200), (600, 200), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (0, 400), (600, 400), LINE_WIDTH)
+    #Vertical lines
+    pygame.draw.line(screen, LINE_COLOR, (200, 0), (200, 600), LINE_WIDTH)
+    pygame.draw.line(screen, LINE_COLOR, (400, 0), (400, 600), LINE_WIDTH)
+draw_lines()
 
 running = True
 while running:
     for event in pygame.event.get():
+        #Setting background
+        screen.blit(background, (0,0))
+        draw_lines()
+        pygame.display.update()
+       
+        #Exiting window
         if event.type == pygame.QUIT:
             running = False
             
